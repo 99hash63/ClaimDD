948b3dd74689a98832222e4dacefd798ca6d38ae
99hash63 <thkottegoda@gmail.com>
2021-11-15 04:50:33 +0530

    updated dashboard with lock-unlock buttons

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  selectClaiamant = 'lock';
+  selectProject = 'lock';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onUnlockClaimant() {
+    this.selectClaiamant = 'unlock';
   }
 
+  onLockClaimant() {
+    this.selectClaiamant = 'lock';
+  }
+
+  onUnlockProject() {
+    this.selectProject = 'unlock';
+  }
+
+  onLockProject() {
+    this.selectProject = 'lock';
+  }
 }
