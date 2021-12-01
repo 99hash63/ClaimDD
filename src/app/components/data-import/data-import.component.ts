2e981b4e40559cbaf6e58577e49ea765a1239af5
99hash63 <thkottegoda@gmail.com>
2021-12-01 23:35:59 +0530

    linked quantum page buutons to import function

--- src/app/components/data-import/data-import.component.ts
+++ src/app/components/data-import/data-import.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { columns } from 'ngx-bootstrap-icons';
 import * as XLSX from 'xlsx';
-import { QuantumResourcesManpowerAdmin } from '../../shared/quantum-resources-manpower-admin.model';
-import { QuantumResourcesManpowerAdminService } from 'src/app/shared/quantum-resources-manpower-admin.service';
+import { QuantumService } from 'src/app/shared/quantum.service';
 import Swal from 'sweetalert2';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-data-import',
   templateUrl: './data-import.component.html',
@@ -14,8 +13,6 @@ export class DataImportComponent implements OnInit {
   data!: [][];
   rowIndex = 0;
   columnIndex = 0;
-  // dateStartIndex = 0;
-  // dateEndIndex = 0;
   placeId = 0;
   isFileUploaded = false;
 
@@ -25,20 +22,14 @@ export class DataImportComponent implements OnInit {
   importStartDate!: Date;
   //import end date
   importEndDate!: Date;
-  //qrma object
-  qrma: QuantumResourcesManpowerAdmin = {
-    resourceId: '',
-    resourceName: '',
-    date: '',
-    project: '',
-    value: 0,
-  };
 
-  constructor(
-    public quantumResourcesManpowerAdminService: QuantumResourcesManpowerAdminService
-  ) {}
+  constructor(public quantumService: QuantumService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.quantumService.importQuantumType == '') {
+      this.router.navigateByUrl('/quantum');
+    }
+  }
 
   onFileChange(evt: any) {
     const traget: DataTransfer = <DataTransfer>evt.target;
@@ -76,48 +67,11 @@ export class DataImportComponent implements OnInit {
     reader.readAsBinaryString(traget.files[0]);
   }
 
-  // incrementRowIndex() {
-  //   this.rowIndex += 1;
-  // }
-
-  // incrementColumnIndex() {
-  //   this.columnIndex += 1;
-  // }
-
-  //set start and end dates and indexes
-  // setDateIndex(index: any) {
-  //   //restrat interface table
-  //   // this.rowIndex = 0;
-  //   // this.columnIndex = 0;
-  //   // alert(this.dateStartIndex);
-
-  //   if (this.dateStartIndex == 0) {
-  //     this.dateStartIndex = index.value;
-  //     this.importStartDate = this.data[0][this.dateStartIndex - 1];
-  //     // console.log(this.importStartDate);
-  //   } else if (this.dateEndIndex == 0) {
-  //     this.dateEndIndex = index.value;
-  //     this.importEndDate = this.data[0][this.dateEndIndex - 1];
-  //   } else {
-  //     return;
-  //   }
-  //   // console.log('start' + this.dateStartIndex);
-  //   // console.log('end' + this.dateEndIndex);
-  // }
-
-  // reset start and end dates
-  // resetStartAndEnd() {
-  //   restrat interface table
-  //   this.rowIndex = 0;
-  //   this.columnIndex = 0;
-
-  //   this.dateStartIndex = 0;
-  //   this.dateEndIndex = 0;
-  // }
   //getting users selected month from dropdown
   onSelectedMonthChange(event: any) {
     this.importMonth = event.target.value;
   }
+
   //data iumport function
   importData() {
     this.data.forEach((row) => {
@@ -135,8 +89,8 @@ export class DataImportComponent implements OnInit {
     }).then((result) => {
       if (result.isConfirmed) {
         //call function in service
-        this.quantumResourcesManpowerAdminService
-          .addQuantumResourcesManpowerAdmin(this.data, this.importMonth)
+        this.quantumService
+          .importQuantumData(this.data, this.importMonth)
           .subscribe(
             (res) => {
               console.log(res['msg']);
--- src/app/shared/quantum-resources-manpower-admin.model.ts
+++
@@ -1,7 +0,0 @@
-export interface QuantumResourcesManpowerAdmin {
-  resourceId: String;
-  resourceName: String;
-  date: any;
-  project: string;
-  value: Number;
-}
